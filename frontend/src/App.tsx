6da4533015b0c3db65d312c7d08ecf4609fd29f4
adarsh946 <adarsh.shukla2724@gmail.com>
2025-02-07 14:51:25 +0530

    profilepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,21 @@
-
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Components/LandingPage.tsx";
 import Layout from "./Components/Layout.tsx";
 import HomeScreen from "./screens/HomeScreen.tsx";
+import ProfilePage from "./Components/ProfilePage.tsx";
 function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Layout/>}>
-            <Route index element={<HomeScreen />}/>
-            <Route path='meet' element={<h2>Meet Here  </h2>}/>
+        <Route path="/home" element={<Layout />}>
+          <Route index element={<HomeScreen />} />
+          <Route path="profile" element={<ProfilePage />} />
         </Route>
-          <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Components/NavigationBar.tsx
+++ frontend/src/Components/NavigationBar.tsx
@@ -1,33 +1,40 @@
-import React from 'react'
+import React from "react";
 
-import { NavLink } from 'react-router-dom';
-import OtherHousesIcon from '@mui/icons-material/OtherHouses';
-import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
-import ModelTrainingIcon from '@mui/icons-material/ModelTraining';
+import { NavLink } from "react-router-dom";
+import OtherHousesIcon from "@mui/icons-material/OtherHouses";
+import IntegrationInstructionsIcon from "@mui/icons-material/IntegrationInstructions";
+import ModelTrainingIcon from "@mui/icons-material/ModelTraining";
 
-const NavbarComponent:React.FC = () => {
-    return (
-        <div className="flex justify-around items-center flex-wrap gap-5 absolute top-0 bg-rose-200 w-full min-h-12">
+const NavbarComponent: React.FC = () => {
+  return (
+    <div className="flex justify-around items-center flex-wrap gap-5 absolute top-0 bg-rose-200 w-full min-h-12">
+      <img className="w-12" src="/Logo.svg" alt="error" />
 
-            <img className="w-12" src="/logo.svg" alt="error"/>
-
-            <div className="flex justify-center items-center flex-wrap gap-5">
-            <NavLink to="/" className=" flex items-center justify-center rounded-lg">
-                <OtherHousesIcon />
-                <span className="btm-nav-label text-xs sm:text-lg md:text-md">Home  </span>
-            </NavLink>
-            <NavLink to="/" className="flex items-center justify-center ">
-                <IntegrationInstructionsIcon/>
-                <span className="btm-nav-label text-xs sm:text-lg md:text-md">My Interviews </span>
-            </NavLink>
-            <NavLink to="/" className="flex items-center justify-center">
-                < ModelTrainingIcon/>
-                <span className="btm-nav-label text-xs sm:text-lg md:text-md">Progress </span>
-            </NavLink>
+      <div className="flex justify-center items-center flex-wrap gap-5">
+        <NavLink
+          to="/"
+          className=" flex items-center justify-center rounded-lg"
+        >
+          <OtherHousesIcon />
+          <span className="btm-nav-label text-xs sm:text-lg md:text-md">
+            Home{" "}
+          </span>
+        </NavLink>
+        <NavLink to="/" className="flex items-center justify-center ">
+          <IntegrationInstructionsIcon />
+          <span className="btm-nav-label text-xs sm:text-lg md:text-md">
+            My Interviews{" "}
+          </span>
+        </NavLink>
+        <NavLink to="/" className="flex items-center justify-center">
+          <ModelTrainingIcon />
+          <span className="btm-nav-label text-xs sm:text-lg md:text-md">
+            Progress{" "}
+          </span>
+        </NavLink>
       </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default NavbarComponent
+export default NavbarComponent;
