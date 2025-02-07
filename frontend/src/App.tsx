cd6c23f0d199d4ca73e072a704eda4148105ffc6
adarsh946 <adarsh.shukla2724@gmail.com>
2025-02-07 21:26:23 +0530

    some changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./Components/SingleComponents/LandingPage.tsx";
 import Layout from "./Components/SingleComponents/Layout.tsx";
 import TeacherDashboard from "./screens/TeacherDashboard.tsx";
@@ -7,34 +6,43 @@ import ProfilePage from "./Components/SingleComponents/ProfilePage.tsx";
 import StudentDashboard from "./screens/StudentDashboard.tsx";
 import NotificationCard from "./Components/SingleComponents/NotificationCard.tsx";
 import ProtectedComponent from "./Components/SingleComponents/ProtectiveComponent.tsx";
-import {useUserStore} from "./store/User.store.ts";
+import { useUserStore } from "./store/User.store.ts";
 
 function App() {
-    const user = useUserStore(state => state.user);
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={user ? <Navigate to="/home"/> : <LandingPage/>}/>
-                <Route path="/home" element={<ProtectedComponent><Layout/></ProtectedComponent>}>
-                    <Route index element={<StudentDashboard/>}/>
-                    <Route path="2" element={<TeacherDashboard/>}/>
-                    <Route path="profile" element={<ProfilePage/>}/>
-                    <Route
-                        path="notification"
-                        element={
-                            <NotificationCard
-                                imageUrl="https://example.com/avatar1.jpg"
-                                message="Dr. Rahela Farooqi posted: DMS Jamia Millia Islamia"
-                                time="6m"
-                            />
-                        }
-                    />
-                </Route>
-                <Route path="*" element={<h1>Not Found</h1>}/>
-            </Routes>
-        </BrowserRouter>
-    );
-
+  const user = useUserStore((state: { user: any }) => state.user);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/home" /> : <LandingPage />}
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedComponent>
+              <Layout />
+            </ProtectedComponent>
+          }
+        >
+          <Route index element={<StudentDashboard />} />
+          <Route path="2" element={<TeacherDashboard />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route
+            path="notification"
+            element={
+              <NotificationCard
+                imageUrl="https://example.com/avatar1.jpg"
+                message="Dr. Rahela Farooqi posted: DMS Jamia Millia Islamia"
+                time="6m"
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
