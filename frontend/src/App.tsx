75b293b91979a22a6abf8c221f0d76ca16ebce1f
adarsh946 <adarsh.shukla2724@gmail.com>
2025-02-07 19:31:13 +0530

    loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Components/LandingPage.tsx";
 import Layout from "./Components/Layout.tsx";
 import TeacherDashboard from "./screens/TeacherDashboard.tsx";
@@ -7,22 +7,29 @@ import StudentDashboard from "./screens/StudentDashboard.tsx";
 import NotificationCard from "./Components/NotificationCard.tsx";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LandingPage/>}/>
-                <Route path="/home" element={<Layout/>}>
-                    <Route index element={<StudentDashboard/>}/>
-                    <Route path="2" element={<TeacherDashboard/>}/>
-                    <Route path="profile" element={<ProfilePage/>}/>
-                    <Route path='notification' element={<NotificationCard imageUrl="https://example.com/avatar1.jpg"
-                                                                          message="Dr. Rahela Farooqi posted: DMS Jamia Millia Islamia"
-                                                                          time="6m"/>}/>
-                </Route>
-                <Route path="*" element={<h1>Not Found</h1>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Layout />}>
+          <Route index element={<StudentDashboard />} />
+          <Route path="2" element={<TeacherDashboard />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route
+            path="notification"
+            element={
+              <NotificationCard
+                imageUrl="https://example.com/avatar1.jpg"
+                message="Dr. Rahela Farooqi posted: DMS Jamia Millia Islamia"
+                time="6m"
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
